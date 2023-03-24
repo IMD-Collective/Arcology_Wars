8956231864593547b91fbd83c8a5650bcec504f9
NiloticNarcotic <70078769+NiloticNarcotic@users.noreply.github.com>
2023-03-24 15:33:55 -0700

    Arcology Wars
    - Ported over all of the AW main content.

---
+++ common/defines/AW_defines.lua
@@ -0,0 +1,16 @@
+NDefines.NGame.START_DATE = "2008.1.1.12"
+NDefines.NGame.END_DATE = "2036.1.1.1"
+NDefines.NFocus.FOCUS_POINT_DAYS = 1						-- Each point takes a week
+
+NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_STATE_G = 0.33
+NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_R = 1.9
+NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_G = 0.16
+NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_B = 0.16
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_TEX_MAX_SIZE = 2048 -- Tweak dependly on amount of countries. Must be power of 2. No more then 2048.
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_SMALL_TEX_MAX_SIZE = 512 -- Tweak dependly on amount of countries. Must be power of 2. No more then 2048.
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_WIDTH = 10
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_HEIGHT = 8196
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_WIDTH = 41
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_HEIGHT = 24000
+
+NDefines.NBuildings.INFRASTRUCTURE_RESOURCE_BONUS = 0.0
